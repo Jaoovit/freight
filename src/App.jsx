a29826c0094441438ea6d99da9846c273fddc84c
João Oliveira <jaoovit@gmail.com>
2024-11-25 15:52:44 +0000

    config react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+{/*Hook to navigate in the pages*/}
+import { Outlet } from 'react-router-dom'
+
+{/*Css*/}
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <Outlet />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+{/*React Hookes*/}
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+{/*Css*/}
+import './index.css'
+
+{/*JSX files*/}
+import App from './App.jsx'
+import Home from './pages/Home.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+        <Route path='/'element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
